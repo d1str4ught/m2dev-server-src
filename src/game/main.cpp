286439aada0f23e0f6999c0dd2e0b5a3de4ef58f
savis <106487343+savisxss@users.noreply.github.com>
2025-09-01 19:19:42 +0200

    removed teen_packet

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -797,18 +797,14 @@ int start(int argc, char **argv)
 			fprintf(stderr, "MasterAuth %d", LC_GetLocalType());
 		}
 	}
-	/* game server to teen server */
 	else
 	{
-		if (teen_addr[0] && teen_port)
-			g_TeenDesc = DESC_MANAGER::instance().CreateConnectionDesc(main_fdw, teen_addr, teen_port, PHASE_TEEN, true);
-
 		extern unsigned int g_uiSpamBlockDuration;
 		extern unsigned int g_uiSpamBlockScore;
 		extern unsigned int g_uiSpamReloadCycle;
 
 		sys_log(0, "SPAM_CONFIG: duration %u score %u reload cycle %u\n",
-				g_uiSpamBlockDuration, g_uiSpamBlockScore, g_uiSpamReloadCycle);
+			g_uiSpamBlockDuration, g_uiSpamBlockScore, g_uiSpamReloadCycle);
 
 		extern void LoadSpamDB();
 		LoadSpamDB();
@@ -1025,21 +1021,6 @@ int io_loop(LPFDWATCH fdw)
 				else if (d->ProcessOutput() < 0)
 				{
 					d->SetPhase(PHASE_CLOSE);
-				}
-				else if (g_TeenDesc==d)
-				{
-					int buf_size = buffer_size(d->GetOutputBuffer());
-					int sock_buf_size = fdwatch_get_buffer_size(fdw, d->GetSocket());
-
-					int ret = d->ProcessOutput();
-
-					if (ret < 0)
-					{
-						d->SetPhase(PHASE_CLOSE);
-					}
-
-					if (buf_size)
-						sys_log(0, "TEEN::Send(size %d sock_buf %d ret %d)", buf_size, sock_buf_size, ret);
 				}
 				break;
 
