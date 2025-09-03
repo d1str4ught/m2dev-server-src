3f8e36919384dc7b4e7dbd6fdc1fd7631b474582
irawork <iraworkcoder@gmail.com>
2025-09-03 03:10:38 +0300

    VCard Remove

--- src/db/ClientManager.h
+++ src/db/ClientManager.h
@@ -369,10 +369,6 @@ class CClientManager : public CNetBase, public singleton<CClientManager>
 	void		DeleteObject(DWORD dwID);
 	void		UpdateLand(DWORD * pdw);
 
-	// VCard
-	void 		VCard(TPacketGDVCard * p);
-	void		VCardProcess();
-
 	// BLOCK_CHAT
 	void		BlockChat(TPacketBlockChat * p);
 	// END_OF_BLOCK_CHAT
@@ -423,8 +419,6 @@ class CClientManager : public CNetBase, public singleton<CClientManager>
 	std::vector<building::TLand>		m_vec_kLandTable;
 	std::vector<building::TObjectProto>	m_vec_kObjectProto;
 	std::map<DWORD, building::TObject *>	m_map_pkObjectTable;
-
-	std::queue<TPacketGDVCard>		m_queue_vcard;
 
 	bool					m_bShutdowned;
 
