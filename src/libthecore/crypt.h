1e88ce3ed6d788bd57f42c51d12a36680dee6a39
rtw1x1 <off.tw1x1@gmail.com>
2026-02-03 19:27:15 +0000

    Clean libthecore

--- src/libthecore/crypt.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-
-int GOST_Encrypt(DWORD * DstBuffer, const DWORD * SrcBuffer, const DWORD * KeyAddress, DWORD Length, DWORD *IVector);
-int GOST_Decrypt(DWORD * DstBuffer, const DWORD * SrcBuffer, const DWORD * KeyAddress, DWORD Length, DWORD *IVector);
-
-int DES_Encrypt(DWORD *DstBuffer, const DWORD * SrcBuffer, const DWORD *KeyAddress, DWORD Length, DWORD *IVector);
-int DES_Decrypt(DWORD *DstBuffer, const DWORD * SrcBuffer, const DWORD *KeyAddress, DWORD Length, DWORD *IVector);
