db87d068bc4ac68dc8fc12a5406ec7c30a6e30c3
rtw1x1 <off.tw1x1@gmail.com>
2026-02-03 11:11:37 +0000

    XChaCha20-Poly1305 via libsodium

--- src/libthecore/crypt.h
+++ src/libthecore/crypt.h
@@ -1,16 +1,5 @@
 ﻿#pragma once
 
-/* TEA is a 64-bit symmetric block cipher with a 128-bit key, developed
-    by David J. Wheeler and Roger M. Needham, and described in their
-    paper at <URL:http://www.cl.cam.ac.uk/ftp/users/djw3/tea.ps>.
-
-    This implementation is based on their code in
-    <URL:http://www.cl.cam.ac.uk/ftp/users/djw3/xtea.ps> 
-*/
-
-int TEA_Encrypt(DWORD *dest, const DWORD *src, const DWORD *key, int size);
-int TEA_Decrypt(DWORD *dest, const DWORD *src, const DWORD *key, int size);
-
 int GOST_Encrypt(DWORD * DstBuffer, const DWORD * SrcBuffer, const DWORD * KeyAddress, DWORD Length, DWORD *IVector);
 int GOST_Decrypt(DWORD * DstBuffer, const DWORD * SrcBuffer, const DWORD * KeyAddress, DWORD Length, DWORD *IVector);
 
